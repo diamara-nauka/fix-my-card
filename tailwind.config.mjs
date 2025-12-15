5dfa3f9ff98ae2aff5ee09f59b7e7db8f9db9f85
Julien Vesy <julien.vesy@smile.fr>
2025-12-15 11:22:47 +0100

    remove header js

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,15 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
+      keyframes: {
+        'slide-down': {
+          '0%': { opacity: '0', transform: 'translateY(-10px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        }
+      },
+      animation: {
+        'slide-down': 'slide-down 0.2s ease-out',
+      }
     },
   },
   plugins: [],
